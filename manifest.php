16f29b5497064c1db3aab54b481328fab7d28b01
bugalood <jerome@taotesting.com>
2014-06-26 16:34:26 +0200

    Initial manifest file.

---
+++ manifest.php
@@ -0,0 +1,51 @@
+<?php
+/**  
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * Copyright (c) 2014 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * 
+ */
+
+$extpath = dirname(__FILE__).DIRECTORY_SEPARATOR;
+$taopath = dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR.'tao'.DIRECTORY_SEPARATOR;
+
+return array(
+	'name' => 'ekstera',
+    'label' => 'Ekstera',
+	'description' => 'A Proof of Concept implementation of irtTest',
+    'license' => 'GPL-2.0',
+    'version' => '1.0.0',
+	'author' => 'Open Assessment Technologies',
+	'requires' => array(
+	    'irtTest' => '1.0.0'
+	),
+	'install' => array(
+		'rdf' => array(
+		    dirname(__FILE__).DIRECTORY_SEPARATOR.'install'.DIRECTORY_SEPARATOR.'ontology'.DIRECTORY_SEPARATOR.'ekstera.rdf'
+		),
+		'checks' => array(
+		),
+	),
+    'autoload' => array (
+        'psr-4' => array(
+            'oat\\irtRandomTest\\' => dirname(__FILE__).DIRECTORY_SEPARATOR
+        ),
+    ),
+	'managementRole' => 'http://www.tao.lu/Ontologies/TAOTest.rdf#IRTtestManager',
+    'acl' => array(
+    ),    
+	'constants' => array(
+	)
+);
